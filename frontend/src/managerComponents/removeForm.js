aabd3440f160dbbc29dcc01d02a122cdd85d630e
joshuahillis292002 <joshuahillis292002@tamu.edu>
2022-12-05 21:30:24 -0600

    Added functionality to allow manager to remove menu items

--- frontend/src/managerComponents/removeForm.js
+++ frontend/src/managerComponents/removeForm.js
@@ -1,44 +1,65 @@
 import axios from 'axios';
-import React, {Fragment} from 'react';
-import Button from "@mui/material/Button";
+import React, {useState, useEffect} from 'react';
+import {useNavigate, useParams} from 'react-router-dom';
+import { Translator, Translate } from 'react-auto-translate';
+import {MenuItem, TextField} from "@mui/material";
 
-class RemoveForm extends React.Component {
-    constructor (props) {
-        super(props);
-        this.initialState = {
-            ingredientName:''
+const apiKey = process.env.REACT_APP_GOOGLE_API_KEY;
+
+const RemoveMenuForm = ({lang}) => {
+
+    useEffect(() => {
+        const fetchIngredients = async () => {
+            const menuResponse = await fetch('http://localhost:8000/menu');
+            const menuData = await menuResponse.json();
+
+            set_menu_list(menuData);
+
         };
-        this.state = this.initialState;
-    }
+        fetchIngredients();
+    }, [])
 
-    handleChange = event => {
-        const {name, value} = event.target;
-        this.setState({
-            [name]: value
+    let navigate = useNavigate();
+    const {id} = useParams();
+
+    const [menu_item, set_menu_item] = useState('')
+    const [menuList, set_menu_list] = useState([{'menu_item':''}])
+
+
+    const RemoveMenu = async ({lang}) => {
+        let formField = new FormData();
+        await axios({
+            method: 'delete',
+            url: 'http://localhost:8000/menu/' + menu_item,
+            data: formField
+        }).then(response => {
+            console.log(response.data);
+            window.location.reload(false);
+            navigate('/manager');
         })
     }
 
-    submitForm = () => {
-        this.props.handleSubmit(this.state);
-        this.setState(this.initialState);
+    const handleMenuChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        set_menu_item(event.target.value);
     }
 
-      render (){
-    const {name, job} = this.state;
-
     return (
-      <form>
-            <input
-                type="text"
-                name="ingredientName"
-                value={name}
-                placeholder={"Ingredient Name"}
-                onChange={this.handleChange} />
-            <Button type='submit' id="removeIngrButton" variant="contained" onClick={() => this.submitForm}>Remove</Button>
-      </form>
+        <Translator
+            from='en' to={lang} googleApiKey={apiKey}>
+        <div className="container">
+            <div className="form-group">
+                <b><Translate>Select Menu Item to Remove</Translate></b>
+                <TextField label="Select Menu Item to Remove" select value={menu_item} onChange={handleMenuChange} fullWidth>
+                    {menuList.map(menu => (
+                        <MenuItem value={menu.menu_item}><Translate>{menu.menu_item}</Translate></MenuItem>
+                    ))}
+                </TextField>
+            </div>
+            <button onClick={RemoveMenu} className="btn btn-primary btn-block"><Translate>Remove Menu Item</Translate></button>
+
+        </div>
+        </Translator>
     )
-  }
 }
 
-export default RemoveForm;
-
+export default RemoveMenuForm;

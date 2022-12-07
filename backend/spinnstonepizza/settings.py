9a0423949fdea81af4bd73bc93dd565ae04945e7
ShantanuT01 <shantanut12@tamu.edu>
2022-12-06 18:42:47 -0600

    Update settings.py

--- backend/spinnstonepizza/settings.py
+++ backend/spinnstonepizza/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://spin-n-stone-pos.herokuapp.com"]
+ALLOWED_HOSTS = ["spin-n-stone-pos.herokuapp.com"]
 
 
 # Application definition
